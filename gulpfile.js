662ad88e7221b10713194ca135ba884122bfec3f
dobrapyra <michal.zielinski@crafton.pl>
2018-05-12 08:57:52 +0200

    remove gulp notify

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,7 @@ let gulp = require( 'gulp' ),
   concat = require( 'gulp-concat' ),
   rename = require( 'gulp-rename' ),
   stripdebug = require( 'gulp-strip-debug' ),
-  uglify = require( 'gulp-uglify' ),
-  notify = require( 'gulp-notify' );
-
-let notifyLogOnly = notify.withReporter( ( options, callback ) => {
-  console.log( 'Message:', options.message );
-  callback();
-} );
-notifyLogOnly.logLevel(1);
+  uglify = require( 'gulp-uglify' );
 
 gulp.task( 'buildES5', () => {
   return gulp
@@ -21,7 +14,6 @@ gulp.task( 'buildES5', () => {
     ] )
     .pipe( concat( 'svg-path-parser.js' ) )
     .pipe( gulp.dest( './dist' ) )
-    .pipe( notifyLogOnly( { message: 'ES5 build done!' } ) )
     .pipe( rename( 'svg-path-parser.min.js' ) )
     .pipe( stripdebug() )
     .pipe( uglify( {
@@ -29,10 +21,7 @@ gulp.task( 'buildES5', () => {
         comments: `/^!/`
       }
     } ) )
-    .pipe( gulp.dest( './dist' ) )
-    .pipe( notifyLogOnly( { message: 'ES5 min build done!' } ) )
-    .pipe( notifyLogOnly( { message: 'All done!' } ) );
-  //   .pipe( notify( { message: 'ES5 all build done!' } ) );
+    .pipe( gulp.dest( './dist' ) );
 } );
 
 // default tasks
